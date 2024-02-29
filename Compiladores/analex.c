fb224da194dd768e0cd3d11b1f34115548ae2f5b
Giulia Bagodi <117239479+GiuBagodi@users.noreply.github.com>
2024-02-29 10:02:27 -0300

    Update analex.c

--- Compiladores/analex.c
+++ Compiladores/analex.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "analex.h"
 
+/*
+Codigo testa se analex.h esta funcionando
+mostra se os caracteres estao sendo reconhecidos como tokens
+precisa do ";" para funcionar corretamente (Ex: "5+6;")
+*/
+
 void main() {
   int token;
   int i = 1;
