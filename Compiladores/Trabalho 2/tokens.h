a67e5e993e35fe04663905d11ce64ac7fb01dc74
Giulia Bagodi <117239479+GiuBagodi@users.noreply.github.com>
2024-04-18 10:22:43 -0300

    Retirada de tokens desnecessários

--- Compiladores/Trabalho 2/tokens.h
+++ Compiladores/Trabalho 2/tokens.h
@@ -17,8 +17,8 @@
 #define SHORT 275
 #define UNSIGNED 276
 #define SIGNED 277
-#define PRINTF 278
-#define SCANF 279
+#define SIZEOF 278
+#define AT_MOD 279
 #define FOR 280
 #define AND 281
 #define OR 282
@@ -30,17 +30,14 @@
 #define AT_DIV 288
 #define STRUCT 289
 #define RETURN 290
-#define INCLUDE 291
-#define DEFINE 292
+#define DESL_DIR 291
+#define DESL_ESQ 292
 #define SWITCH 293
 #define CASE 294
 #define BREAK 295
 #define DEFAULT 296
 #define TYPEDEF 297
-#define TRUE 298
-#define FALSE 299
-#define AT_MOD 300
-#define DESL_ESQ 301
-#define DESL_DIR 302
-#define SIZEOF 303
+#define DESL_ESQ 298
+#define DESL_DIR 299
+#define SIZEOF 300
 
